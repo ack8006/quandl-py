d0b5a82c279e3816f17cf188add152895a36e5f1
ack8006 <takata.alex@gmail.com>
2015-09-07 15:42:40 -0400

    formatting

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 
 setup(
-    name='quandl-py'
-    description='simple Quandl python wrapper'
-    long_description = (read('README.rst'))
-    version = 0.1
-    author = 'Alex Takata'
-    url = 'https://github.com/ack8006/quandl-py'
-    license = 'MIT'
-    keywords = 'Quandl Python Wrapper'
+    name='quandl-py',
+    description='simple Quandl python wrapper',
+    long_description = (read('README.rst')),
+    version = 0.1,
+    author = 'Alex Takata',
+    url = 'https://github.com/ack8006/quandl-py',
+    license = 'MIT',
+    keywords = 'Quandl Python Wrapper',
     install_requires = [
         'pandas >= 0.14',
         'requests',
