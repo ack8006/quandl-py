23cc4ee2e0a16d3b3f5f6c24148571c113c338ef
ack8006 <takata.alex@gmail.com>
2015-09-07 16:35:29 -0400

    setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='simple Quandl python wrapper',
     version = 0.2,
     author = 'Alex Takata',
-    author_email = 'takata.alex@gmail.com'
+    author_email = 'takata.alex@gmail.com',
     url = 'https://github.com/ack8006/quandl-py',
     license = 'MIT',
     keywords = 'Quandl Python Wrapper',
