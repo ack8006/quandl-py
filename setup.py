ae5341ae66e40b6254b09a6de89c8e01d5708c56
ack8006 <takata.alex@gmail.com>
2015-09-07 16:21:35 -0400

    setup update

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 
 setup(
     name='quandl-py',
+    packages = ['quandl-py'],
     description='simple Quandl python wrapper',
-    version = 0.1,
+    version = 0.1.1,
     author = 'Alex Takata',
     url = 'https://github.com/ack8006/quandl-py',
     license = 'MIT',
